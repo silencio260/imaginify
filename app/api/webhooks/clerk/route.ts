bb192fff995dcdeb208bf302044cd961ce39ec9a
silencio260 <faruq5840@gmail.com>
2024-07-10 22:55:01 +0100

    Fix error with clerkClient Import

--- app/api/webhooks/clerk/route.ts
+++ app/api/webhooks/clerk/route.ts
@@ -1,6 +1,6 @@
 /* eslint-disable camelcase */
-import { clerkClient } from "@clerk/nextjs";
-import { WebhookEvent } from "@clerk/nextjs/server";
+// import { clerkClient } from "@clerk/nextjs";
+import { clerkClient, WebhookEvent } from "@clerk/nextjs/server";
 import { headers } from "next/headers";
 import { NextResponse } from "next/server";
 import { Webhook } from "svix";
