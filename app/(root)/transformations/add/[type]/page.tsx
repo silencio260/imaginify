335e13ded592f7d18f7594a4d9dea0f7a63d3bf7
silencio260 <faruq5840@gmail.com>
2024-07-11 16:30:51 +0100

    Implement image transformation page and logic

--- app/(root)/transformations/add/[type]/page.tsx
+++ app/(root)/transformations/add/[type]/page.tsx
@@ -1,8 +1,40 @@
+import Header from '@/components/shared/Header'
+import TransformationForm from '@/components/shared/TransformationForm';
+import { transformationTypes } from '@/constants'
+import { getUserById } from '@/lib/actions/user.actions';
+import { auth } from '@clerk/nextjs/server';
+import { redirect } from 'next/navigation';
 import React from 'react'
 
-const AddTransformationTypePage = () => {
+const AddTransformationTypePage = async ({params: {type}} : SearchParamProps) => {
+  
+  const transformation = transformationTypes[type];
+
+  const {userId} = auth()
+
+  if(!userId) redirect('sign-in')
+
+  const user = await getUserById(userId)
+  
   return (
-    <div>AddTransformationTypePage</div>
+    <>
+
+      <Header
+      title={transformation.title}
+      subtitle={transformation.subTitle}
+      />
+
+      <section className='mt-10'>
+        <TransformationForm 
+          action='Add'
+          userId={user._id}
+          type={transformation.type as TransformationTypeKey}
+          creditBalance={user.creditBalance}
+        />
+      </section>
+
+
+    </>
   )
 }
 
